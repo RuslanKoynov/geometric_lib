94590e1f22ef7d30d676e7b451e9b41c1b50ca66
Name <you@example.com>
2023-10-02 19:09:15 +0300

    added documentation for circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,23 @@ import math
 
 
 def area(r):
+    '''
+    Calculates the area of circle
+    :param r: circle radius value
+    :type r: int or float
+    :rtype: float
+    :return: math.pi * r * r - area of a circle
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Calculates the perimeter of circle
+    :param r: circle radius value
+    :type r: int or float
+    :rtype: float
+    :return: 2 * math.pi * r - perimeter of a circle
+    '''
     return 2 * math.pi * r
 

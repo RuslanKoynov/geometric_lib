bc85b5f54aba900b58ba36303c0509112bc4baa7
Name <you@example.com>
2023-10-02 19:10:16 +0300

    added documentation for rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,23 @@
 def area(a, b): 
+    '''
+    Calculates the area of rectangle
+    :param a: size of the side of the rectangle (first side)
+    :type a: int or float
+    :param b: size of the side of the rectangle (adjacent to the first side)
+    :type b: int or float
+    :rtype: int or float
+    :return: a*b - area of the rectangle
+    '''
     return a * b 
 
 def perimeter(a, b): 
+    '''
+    Calculates the perimeter of rectangle
+    :param a: size of the side of the rectangle (first side)
+    :type a: int or float
+    :param b: size of the side of the rectangle (adjacent to the first side)
+    :type b: int or float
+    :rtype: int or float
+    :return: 2*(a+b) - perimeter of the rectangle
+    '''
     return 2*(a + b) 

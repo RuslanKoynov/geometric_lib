ef14ab15e4915d6a06eba1ce14b8078dac8be82d
Name <you@example.com>
2023-10-02 19:10:35 +0300

    added documentation for square.py

--- square.py
+++ square.py
@@ -1,7 +1,21 @@
 
 def area(a):
+    '''
+    Calculates the area of a square
+    :param a: square side size
+    :type a: int or float
+    :rtype: int or float
+    :return: a*a - area of the square
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+    Calculates the perimeter of a square
+    :param a: square side size
+    :type a: int or float
+    :rtype: int or float
+    :return: 4*a - perimeter of the square
+    '''
     return 4 * a

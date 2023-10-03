1046c60d3e81612b20bbbb36ca04340994a6bc3b
egorlim <144596505+egorlim@users.noreply.github.com>
2023-10-03 22:44:38 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,30 @@
 
 def area(a):
+    '''
+    The area(a) function calculates the area of a square.
+    
+    Formula:
+    
+    Area = a * a
+
+    Description:
+    
+    • ‎Area is the area of the square.
+    •‎ a is the length of one side of the square.
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+    The perimeter(a) function calculates the perimeter of a square.
+
+    Formula:
+
+    Perimeter = 4 * a
+
+    Description:
+    •‎ Perimeter is the perimeter of the square.
+    •‎ a is the length of one side of the square.
+    '''
     return 4 * a

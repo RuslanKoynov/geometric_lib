d531677a8a10e7a69ad436e74011bcae6ad1b2bc
PirozhokUbiytsa <vadimskilush@mail.ru>
2023-10-05 00:21:35 +0300

    docs: add comments for circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''принимает число r, возвращает площадь круга радиуса r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''принимает число r, возвращает периметр круга радиуса r'''
     return 2 * math.pi * r
 

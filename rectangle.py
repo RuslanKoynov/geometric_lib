fa132ab5443a55a4ca5bee63765b751a0b1a0a96
mhgffqwoer <a.yakshevich2005@gmail.com>
2023-10-04 21:45:20 +0300

    add documentation

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,25 @@
 
-def area(a, b): 
+def area(a, b):
+    '''Returns the area of the rectangle.
+
+            Parameters:
+                a (int): first side of the rectangle
+                b (int): second side og the rectangle
+            Returns:
+                (int): area of the rectangle
+    '''
+
     return a * b 
 
 
-def perimeter(a, b): 
-    return (a + b) * 2 
+def perimeter(a, b):
+    '''Возвращает периметр прямоугольника.
+
+            Параметры:
+                a (int): первая сторона прямоугольника
+                b (int): вторая сторона прямоугольника.
+            Возврат:
+                (int): периметр прямоугольника
+    '''
+
+    return 2 * (a + b)

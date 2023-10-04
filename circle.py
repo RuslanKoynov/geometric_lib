a4163730eb245e7f39c5af8d89666712beca2a9c
KreezerIT <timslava2005V@ya.ru>
2023-10-04 13:53:29 +0300

    Реализовал раздел docs и сделал комментарии в кодах

--- circle.py
+++ circle.py
@@ -1,10 +1,27 @@
 import math
 
-
 def area(r):
+    """
+    Функция, которая вычисляет площадь круга по его радиусу.
+
+    Args:
+        r (float): Радиус круга.
+
+    Returns:
+        float: Площадь круга.
+    """
     return math.pi * r * r
 
-
 def perimeter(r):
+    """
+    Функция, которая вычисляет периметр окружности по ее радиусу.
+
+    Args:
+        r (float): Радиус окружности.
+
+    Returns:
+        float: Периметр окружности.
+    """
     return 2 * math.pi * r
+
 

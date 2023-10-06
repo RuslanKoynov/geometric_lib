9364f27e27e436ea3081ba2a5fbfc21275101761
mefedraw <fyodor.dubkov@gmail.com>
2023-10-06 08:39:51 +0300

    comment block added

--- circle.py
+++ circle.py
@@ -4,8 +4,8 @@ import math
 def area(r):
     return math.pi * r * r
 '''принимает число r - радиус окружности, возвращает площадь окружности'''
-
+print(area(1))
 def perimeter(r):
     return 2 * math.pi * r
 '''принимает число r - радиус окружности, возвращает периметр окружности'''
-
+print(perimeter(1))

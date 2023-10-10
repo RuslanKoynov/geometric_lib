df1eca12fc895ca99a6e0aa8006b65a18f61c900
Popkov Alexey <gercalander@gmail.com>
2023-10-10 16:50:47 +0300

    added comment in Rectangle1.py, Triangle1.py, circle.py, square.py

--- Rectangle1.py
+++ Rectangle1.py
@@ -1,5 +1,7 @@
 def area(a, b): 
+    '''принимает числа a и b, возвращает произведение a и b'''
     return a * b 
 
-def perimeter(a, b): 
-    return a + b 
+def perimeter(a, b):
+    '''принимает числа a и b, возвращает сумму a и b умноженную на 2 ''' 
+    return 2*(a + b) 
--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''принимает число r, возвращает число pi, умноженное на r**2'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''принимает число r, возвращает число pi умноженное на 2 и на r '''
     return 2 * math.pi * r
 

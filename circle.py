b7c6d0f07ebf5d271b702c420876a37fdc58d657
bemooooooooo <103956922+bemooooooooo@users.noreply.github.com>
2023-10-23 12:58:01 +0300

    Added examples at circle.py

--- circle.py
+++ circle.py
@@ -8,6 +8,7 @@ def area(r):
     :type r: int or float
     :rtype: float
     :return: math.pi * r * r - area of a circle
+    :exampl: area(3) = 28.274333882308138
     '''
     return math.pi * r * r
 
@@ -19,6 +20,7 @@ def perimeter(r):
     :type r: int or float
     :rtype: float
     :return: 2 * math.pi * r - perimeter of a circle
+    :example: preimeter(3) = 18.84955592153876
     '''
     return 2 * math.pi * r
 

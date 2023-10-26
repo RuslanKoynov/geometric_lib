f47784c5e052b6e8c04f59d4a9a44b546af92952
Daniil <you@example.com>
2023-10-26 12:27:51 +0300

    docs: add rectangle.py docs

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,8 @@
 def area(a, b):
+    '''Returns area of a rectangle with a and b sides'''
     return a * b;
 
 def perimeter(a, b):
+    '''Returns perimeter of a rectangle with a and b sides'''
     return 2 * (a + b);
 

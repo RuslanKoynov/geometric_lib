152fc0540d7cfb1a7f473d02fd6b5b1923cc33d8
Daniil <you@example.com>
2023-10-26 12:37:08 +0300

    docs: add triangle.py docs

--- triangle.py
+++ triangle.py
@@ -1,6 +1,8 @@
 def area(a, h):
+    '''Returns area of a triangle where b is the length of the base of the triangle, and h is the height'''
     return a * h / 2;
 
 def perimeter(a, b, c):
+    '''Returns perimeter of a triangle with sides a, b, and c'''
     return a + b + c;
 

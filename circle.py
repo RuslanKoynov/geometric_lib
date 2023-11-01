cb5c7d24056f4052b5ae21c21341186d70e4f89e
Ruslan Sanigullin <409512@niuitmo.ru>
2023-11-02 00:55:12 +0300

    docs: added documentation to circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,16 @@
 import math
-
-
+'''Принимает число r и возвращает площадь круга'''
 def area(r):
     return math.pi * r * r
-
-
+'''Пример вызова:
+    result = area(5)
+    print(result) # Выведет 78.5'''
+    
+'''Приннимает число r и возвращает периметр круга'''
 def perimeter(r):
     return 2 * math.pi * r
-
+'''Пример вызова:
+    result = perimetr(5)
+    print(result) # Выведет 31.4'''
+    
+    

f6660d0d6aad6f5d978b6a73dbc9ad0492b9d8f0
BORONINN <hlupikaz@gmail.com>
2023-11-01 15:47:32 +0300

    added tests

--- square.py
+++ square.py
@@ -1,4 +1,16 @@
+import unittest
+class SquareTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+       res = area(0)
+       self.assertEqual(res, 0)
+       
+    def test_area(self):
+       res = area(10)
+       self.assertEqual(res, 100)
 
+    def test_perimeter(self):
+       res = perimeter(7)
+       self.assertEqual(res, 28)
 def area(a):
     ''' Принимает длинну стороны квадрата - a, возвращает площадь квадрата со стороной а '''
     return a * a

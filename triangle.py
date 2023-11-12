135a79b75a7761a4c44c73e8296f6882cf846448
Pr1t3 <romapro2005@gmail.com>
2023-11-13 01:00:50 +0300

    added test7 triangle.py

--- triangle.py
+++ triangle.py
@@ -46,3 +46,6 @@ class TriangleTestCase(unittest.TestCase):
     def test_zero_perim(self):
         res = perimeter(0, 0, 0)
         self.assertEqual(res, 0)
+    def test_small_numbers_perim(self):
+        res = perimeter(15, 10, 5)
+        self.assertEqual(res, 15+10+5)

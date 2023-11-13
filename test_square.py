1c6c5d3848920177b06c6b24274d6c6d00f2e9e2
Ivan Chirkov <ivan.tchirkov@gmail.com>
2023-11-13 07:04:19 +0300

    Add Tests

---
+++ test_square.py
@@ -0,0 +1,48 @@
+import unittest
+from square import area
+from square import perimeter
+
+class SquareTestCase(unittest.TestCase):
+    #Проверка на нулевые входные данные для площади
+    def test_zero_mul_area(self):
+        res = area(0)
+        expected_result = 0
+        self.assertEqual(res, expected_result)
+
+    #Проверка на нулевые входные данные для периметра
+    def test_zero_mul_perimetr(self):
+        res = perimeter(0)
+        expected_result = 0
+        self.assertEqual(res, expected_result)
+
+    #Рандомные числа для площади
+    def test_random_area_1(self):
+        res = area(10)
+        expected_result = 100
+        self.assertEqual(res, expected_result)
+
+    def test_random_area_2(self):
+        res = area(238)
+        expected_result = 56644
+        self.assertEqual(res, expected_result)
+
+    def test_random_area_3(self):
+        res = area(9384)
+        expected_result = 88059456
+        self.assertEqual(res, expected_result)
+
+    #Рандомные числа для периметра
+    def test_random_perimeter_1(self):
+        res = perimeter(10)
+        expected_result = 100
+        self.assertEqual(res, expected_result)
+
+    def test_random_perimeter_2(self):
+        res = perimeter(238)
+        expected_result = 56644
+        self.assertEqual(res, expected_result)
+
+    def test_random_perimeter_3(self):
+        res = perimeter(9384)
+        expected_result = 88059456
+        self.assertEqual(res, expected_result)

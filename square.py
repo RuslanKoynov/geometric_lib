15ba4de4bd961c61b686b43fbb7bd7408cd74382
nekkkkate <144551827+nekkkkate@users.noreply.github.com>
2023-11-14 22:32:38 +0300

    square.py has first tests

--- square.py
+++ square.py
@@ -1,7 +1,10 @@
-
+print('введите сторону а: ')
+a=int(input())
 def area(a):
     return a * a
 
 
 def perimeter(a):
     return 4 * a
+print('площадь квадрата равна ',area(a))
+print('периметр квадрата равен ',perimeter(a))

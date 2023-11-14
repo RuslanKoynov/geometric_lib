2051f58a50689489cf84a403d95ddac6d3fb29e6
nekkkkate <144551827+nekkkkate@users.noreply.github.com>
2023-11-15 01:31:17 +0300

    unit tests added

--- circle.py
+++ circle.py
@@ -1,12 +1,22 @@
 import math
-print('Введите радиус r: ')
-r=int(input())
+import unittest
 def area(r):
     return math.pi * r * r
-
-
 def perimeter(r):
     return 2 * math.pi * r
+class SquareTestCase(unittest.TestCase):
+   def test_zero_mul(self):
+       res = area(0)
+       self.assertEqual(res, 0)
+   def test_curcle_mul(self):
+       res = area(6)
+       self.assertEqual(res, 113.09733552923255)
 
-print('площадь равна', area(r))
-print('периметр равен ', perimeter(r))
+   def test_zero_per(self):
+       res = perimeter(0)
+       self.assertEqual(res, 0)
+
+   def test_circle_per(self):
+       res = perimeter(6)
+       self.assertEqual(res, 37.69911184307752)
+
--- rectangle.py
+++ rectangle.py
@@ -1,18 +1,21 @@
 import unittest
-
 def area(a, b): 
     return a * b
-
 def perimeter(a, b): 
     return (a + b)*2
-
-
 class RectangleTestCase(unittest.TestCase):
-    
    def test_zero_mul(self):
        res = area(10, 0)
        self.assertEqual(res, 0)
-       
-   def test_square_mul(self):
+   def test_rectangle_mul(self):
        res = area(10, 10)
        self.assertEqual(res, 100)
+
+   def test_zero_per(self):
+       res = perimeter(10, 0)
+       self.assertEqual(res, 20)
+
+   def test_rectangle_per(self):
+       res = perimeter(10, 15)
+       self.assertEqual(res, 50)
+

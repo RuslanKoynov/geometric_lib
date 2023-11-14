c8f21ee1f2624cb29a57abea49b1f0c2e3d9f8f0
nekkkkate <144551827+nekkkkate@users.noreply.github.com>
2023-11-14 22:39:55 +0300

    triangle.py has first tests

--- triangle.py
+++ triangle.py
@@ -1,5 +1,15 @@
+print('введите сторону треугольника a: ')
+a=int(input())
+print('введите сторону треугольника b: ')
+b=int(input())
+print('введите сторону треугольника c: ')
+c=int(input())
+print('введите высоту треугольника h: ')
+h=int(input())
 def area(a, h): 
     return a * h / 2 
 
 def perimeter(a, b, c): 
     return a + b + c
+print('площадь равна ', area(a,h))
+print('периметр равен ', perimeter(a,b,c))

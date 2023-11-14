0d8e98dca234805fa3aded9662774acae72dee1f
nekkkkate <144551827+nekkkkate@users.noreply.github.com>
2023-11-15 00:09:14 +0300

    rectangle.py add unittests

--- rectangle.py
+++ rectangle.py
@@ -1,11 +1,17 @@
-print('введите сторону a')
-a=int(input())
-print('введите сторону b')
-b=int(input())
+import unittest
+
+class RectangleTestCase(unittest.TestCase):
+    
+   def test_zero_mul(self):
+       res = area(10, 0)
+       self.assertEqual(res, 0)
+       
+   def test_square_mul(self):
+       res = area(10, 10)
+       self.assertEqual(res, 100)
+
 def area(a, b): 
     return a * b 
 
 def perimeter(a, b): 
     return (a + b)*2
-print('площадь равна ',area(a,b))
-print('периметр равен', perimeter(a,b))

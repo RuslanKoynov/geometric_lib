e72601c0733618419c2c124f15d2617fca67a944
vikaaaleks <vikaaaleks@mail.ru>
2023-11-14 21:53:12 +0300

    added tests for square

---
+++ squaretest.py
@@ -0,0 +1,34 @@
+import unittest
+from square import area
+from square import perimeter
+
+class RectangleTestCase(unittest.TestCase):
+    def test_area_with_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area_with_square(self):
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_area_with_random_number(self):
+        res=area(28)
+        self.assertEqual(res, 784)
+
+    def test_perimeter_with_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_perimeter_with_square(self):
+        res = perimeter(10)
+        self.assertEqual(res, 40)
+
+    def test_perimeter_with_random_number(self):
+        res=perimeter(31)
+        self.assertEqual(res,124)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+

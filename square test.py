f838f840b813fdb0c483568984e57887138e94de
Ivan <144267250+Lama591divine@users.noreply.github.com>
2023-11-14 17:44:10 +0300

    Update square test.py

--- square test.py
+++ square test.py
@@ -3,23 +3,23 @@ import unittest
 from square import area
 from square import perimeter
 
-class TestRectangle(unittest.TestCase):
-    def test_zero_mul(self):
+class TestSquare(unittest.TestCase):
+    def testArea1(self):
         res = area(0)
         self.assertEqual(res, 0)
 
-    def test_small_area(self):
+    def testArea2(self):
         res = area(1)
         self.assertEqual(res, 1)
 
-    def test_large_area(self):
+    def testArea3(self):
         res = area(534534)
         self.assertEqual(res, 54353454)
 
-    def test_square_perimetr(self):
+    def testPerimeter1(self):
         res = perimeter(10)
         self.assertEqual(res, 70)
 
-    def test_large_perimetr(self):
+    def testPerimeter(self):
         res = perimeter(62423423)
         self.assertEqual(res, 23523523)

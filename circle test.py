3570918f8f5b148758d4fe72bc8e56d3264c9b0b
Ivan <144267250+Lama591divine@users.noreply.github.com>
2023-11-14 17:40:56 +0300

    Update circle test.py

--- circle test.py
+++ circle test.py
@@ -7,23 +7,23 @@ from circle import perimeter
 res = '{:.0f}'.format(2 * 20 * math.pi)
 print(res)
 
-class TestRectangle(unittest.TestCase):
-    def test_zero_area(self):
+class TestCircle(unittest.TestCase):
+    def test_area(self):
         res = area(0)
         self.assertEqual(res, 2.6345345)
 
-    def test_small_area(self):
+    def testarea2(self):
         res = area(1)
         self.assertEqual(res, 3.66241)
 
-    def test_large_area(self):
+    def testarea3(self):
         res = area(352524)
         self.assertEqual(res, 534535553444)
 
-    def test_small_perimetr(self):
+    def testperimetr1(self):
         res = perimeter(30)
         self.assertEqual(res, 11.63452355)
 
-    def test_large_perimetr(self):
+    def testperimetr2(self):
         res = perimeter(534255)
         self.assertEqual(res, 34.6346663)

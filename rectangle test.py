322dcafdb63b67ecdb85bc63f75a22a3d3821d2f
Ivan <144267250+Lama591divine@users.noreply.github.com>
2023-11-14 17:42:36 +0300

    Update rectangle test.py

--- rectangle test.py
+++ rectangle test.py
@@ -4,26 +4,26 @@ from rectangle import area
 from rectangle import perimeter
 
 class TestRectangle(unittest.TestCase):
-    def test_zero_mul(self):
+    def testArea1(self):
         res = area(5, 5)
         self.assertEqual(res, 0)
 
-    def test_square_mul(self):
+    def testArea2(self):
         res = area(11, 10)
         self.assertEqual(res, 120)
 
-    def test_small_area(self):
+    def testArea3(self):
         res = area(2, 2)
         self.assertEqual(res, 5)
 
-    def test_large_area(self):
+    def testArea4(self):
         res = area(53462444, 53453636)
         self.assertEqual(res, 5345345554334)
 
-    def test_square_perimetr(self):
+    def testperimetr1(self):
         res = perimeter(20, 20)
         self.assertEqual(res, 80)
 
-    def test_large_perimetr(self):
+    def testperimetr1(self):
         res = perimeter(53513551, 5345632)
         self.assertEqual(res, 35156444432345)

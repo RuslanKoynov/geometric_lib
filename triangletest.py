52c54d89d3fcf7d9578029358eb347f25cb2a068
Maria Safronova <s4fronova.44@yandex.ru>
2023-11-15 00:58:58 +0300

    test:triangle

---
+++ triangletest.py
@@ -0,0 +1,15 @@
+import unittest
+from triangle import area
+from triangle import perimeter
+
+class TriangleTestCase(unittest.TestCase):
+    def test_int(self):
+        self.assertEqual(area(5,7),5*7/2)
+        self.assertEqual(perimeter(19,4,3),19+4+3)
+    
+    def test_float(self):
+        self.assertEqual(area(10.5,12.5),10.5*12.5/2)
+        self.assertEqual(perimeter(5.5,6.7,98.5),5.5+6.7+98.5)
+    def test_int_and_float(self):
+        self.assertEqual(area(6,12.5),6*12.5/2)
+        self.assertEqual(perimeter(55,67,98.5),55+67+98.5)

0e74520fed0d6331766d057e21df41ed94a4caeb
nekkkkate <144551827+nekkkkate@users.noreply.github.com>
2023-11-14 22:29:31 +0300

    rectangle.py has first tests

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,11 @@
+print('введите сторону a')
+a=int(input())
+print('введите сторону b')
+b=int(input())
 def area(a, b): 
     return a * b 
 
 def perimeter(a, b): 
     return (a + b)*2
+print('площадь равна ',area(a,b))
+print('периметр равен', perimeter(a,b))

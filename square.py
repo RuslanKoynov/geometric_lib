7a881d490273df800f5a25fec99070216b12a5f2
monak23 <monak.v@yandex.ru>
2023-11-15 23:01:55 +0300

    Lab4

--- square.py
+++ square.py
@@ -1,7 +1,33 @@
+import unittest
 
 def area(a):
+    '''
+    Принимает число, которое является стороной квадрата: a
+    Возваращает площадь квадрата, вычисляя ее по формуле: a^2
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+    Принимает число, которое является стороной квадрата: a
+    Возваращает периметр квадрата, вычисляя его по формуле: 4 * a
+    '''
     return 4 * a
+
+class SquareTestCase(unittest.TestCase):
+    def test_area_1(self):
+        res = area(1)
+        self.assertEqual(res, 1)
+
+    def test_area_2(self):
+        res = area(2)
+        self.assertEqual(res, 4)
+
+    def test_perimetr_1(self):
+        res = perimeter(1)
+        self.assertEqual(res, 4)
+
+    def test_perimetr_3(self):
+        res = perimeter(3)
+        self.assertEqual(res, 12)

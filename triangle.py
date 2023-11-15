14814d35bde98b9451a4af6ce5c6b1ffca64bfc4
Teaaaaa1 <144700106+Teaaaaa1@users.noreply.github.com>
2023-11-15 14:00:51 +0300

    hasp;dkfj

--- triangle.py
+++ triangle.py
@@ -1,5 +1,11 @@
-def area(a, h): 
-    return a * h / 2 
-
-def perimeter(a, b, c): 
-    return a + b + c 
+def primlenakr(k,a,b):
+    kr = k**2
+    prim = a*b
+    return prim * kr
+def sortirovka(a):
+    a = sorted(a)
+    return a
+def maxChislo(a):
+    a = sortirovka(a)
+    a = a[len(a)-1]
+    return a

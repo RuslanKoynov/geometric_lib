b886fcb9cdd4436347c41965fe0b717b9d236c33
shvedovaalina <shvedova_alinochka13@mail.ru>
2023-11-15 21:30:45 +0300

    tests done for triangle

--- triangle.py
+++ triangle.py
@@ -1,3 +1,5 @@
+import unittest
+
 def area(a, h):
     '''
     Возвращает площадь треугольника
@@ -27,3 +29,47 @@ def perimeter(a, b, c):
     '''
     
      return a + b + c 
+
+class TriangleTestCase(unittest.TestCase):
+
+    def test_area_positive_values(self):
+        res = area(5, 10)
+        self.assertEqual(res, 25)
+
+    def test_area_negative_values(self):
+        res = area(-3, 8)
+        self.assertEqual(res, 0)
+
+    def test_perimeter_zero_values(self):
+        res = perimeter(0, 0, 0)
+        self.assertEqual(res, 0)
+
+    def test_perimeter_mixed_values(self):
+        res = perimeter(3, 4, 5)
+        self.assertEqual(res, 12)
+
+    def test_area_float_values(self):
+        res = area(2.5, 4)
+        self.assertAlmostEqual(res, 5, places=2)
+
+    def test_perimeter_float_values(self):
+        res = perimeter(3.5, 6, 8)
+        self.assertAlmostEqual(res, 17.5, places=2) 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    

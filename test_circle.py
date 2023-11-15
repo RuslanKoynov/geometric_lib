2ba40690859297343a0b546812e1c139114e79b7
KreezerIT <timslava2005V@ya.ru>
2023-11-15 14:22:04 +0300

    Исправлена ошибка в тесте функций окружности

--- test_circle.py
+++ test_circle.py
@@ -1,26 +1,26 @@
 import unittest
 import circle
-
+import math
 class CircleTestCase (unittest.TestCase):
     def test_circle_area_1(self):
-        self.assertEqual(circle.area(3),28.274,places=3)
+        self.assertAlmostEqual(circle.area(3),math.pi * 3 * 3)
 
     def test_circle_area_2(self):
-        self.assertEqual(circle.area(4),50.265,places=3)
+        self.assertAlmostEqual(circle.area(4),math.pi * 4 * 4)
 
     def test_circle_area_3(self):
-        self.assertEqual(circle.area(5),78.539,places=3)
+        self.assertAlmostEqual(circle.area(5),math.pi * 5 * 5)
 
 
 
     def test_circle_perimeter_1(self):
-        self.assertEqual(circle.perimeter(3),18.849,places=3)
+        self.assertAlmostEqual(circle.perimeter(3),2 * math.pi * 3)
 
     def test_circle_perimeter_2(self):
-        self.assertEqual(circle.perimeter(4),25.132,places=3)
+        self.assertAlmostEqual(circle.perimeter(4),2 * math.pi * 4)
 
     def test_circle_perimeter_3(self):
-        self.assertEqual(circle.perimeter(5),31.415,places=3)
+        self.assertAlmostEqual(circle.perimeter(5),2 * math.pi * 5)
 
 
 if __name__ == '__main__':

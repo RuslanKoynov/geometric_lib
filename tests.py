d4911446e494c11feb7efb6be75b568e079941d8
beth <102821957+bethqqe@users.noreply.github.com>
2023-11-15 12:26:50 +0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -30,76 +30,76 @@ class TestCircle(unittest.TestCase):
 
 class TestRectangle(unittest.TestCase):
     def test_zero_area(self):
-        res6 = rectangle_area(10, 0)
-        self.assertEqual(res6, 0)
+        res1 = rectangle_area(10, 0)
+        self.assertEqual(res1, 0)
 
     def test_square_area(self):
-        res7 = rectangle_area(10, 10)
-        self.assertEqual(res7, 100)
+        res2 = rectangle_area(10, 10)
+        self.assertEqual(res2, 100)
 
     def test_small_area(self):
-        res8 = rectangle_area(1, 1)
-        self.assertEqual(res8, 1)
+        res3 = rectangle_area(1, 1)
+        self.assertEqual(res3, 1)
 
     def test_large_area(self):
-        res9 = rectangle_area(123456789, 987654321)
-        self.assertEqual(res9, 121932631112635269)
+        res4 = rectangle_area(123456789, 987654321)
+        self.assertEqual(res4, 121932631112635269)
 
     def test_square_perimeter(self):
-        res10 = rectangle_perimeter(20, 20)
-        self.assertEqual(res10, 80)
+        res5 = rectangle_perimeter(20, 20)
+        self.assertEqual(res5, 80)
 
     def test_large_perimeter(self):
-        res11 = rectangle_perimeter(75384901072, 29813745390)
-        self.assertEqual(res11, 210397292924)
+        res6 = rectangle_perimeter(75384901072, 29813745390)
+        self.assertEqual(res6, 210397292924)
 
 
 class TestSquare(unittest.TestCase):
     def test_zero_area(self):
-        res12 = square_area(0)
-        self.assertEqual(res12, 0)
+        res1 = square_area(0)
+        self.assertEqual(res1, 0)
 
     def test_small_area(self):
-        res13 = square_area(1)
-        self.assertEqual(res13, 1)
+        res2 = square_area(1)
+        self.assertEqual(res2, 1)
 
     def test_large_area(self):
-        res14 = square_area(10)
-        self.assertEqual(res14, 100)
+        res3 = square_area(10)
+        self.assertEqual(res3, 100)
 
     def test_zero_perimeter(self):
-        res15 = square_perimeter(0)
-        self.assertEqual(res15, 0)
+        res4 = square_perimeter(0)
+        self.assertEqual(res4, 0)
 
     def test_small_perimeter(self):
-        res16 = square_perimeter(2)
-        self.assertEqual(res16, 8)
+        res5 = square_perimeter(2)
+        self.assertEqual(res5, 8)
 
     def test_large_perimeter(self):
-        res17 = square_perimeter(15)
-        self.assertEqual(res17, 60)
+        res6 = square_perimeter(15)
+        self.assertEqual(res6, 60)
 
 
 class TestTriangle(unittest.TestCase):
     def test_a_zero_area(self):
-        res18 = triangle_area(0, 10)
-        self.assertEqual(res18, 0)
+        res1 = triangle_area(0, 10)
+        self.assertEqual(res1, 0)
 
     def test_b_zero_area(self):
-        res19 = triangle_area(10, 0)
-        self.assertEqual(res19, 0)
+        res2 = triangle_area(10, 0)
+        self.assertEqual(res2, 0)
 
     def test_large_area(self):
-        res20 = triangle_area(5879324522, 134513256136)
-        self.assertEqual(res20, 395423542667225858048)
+        res3 = triangle_area(5879324522, 134513256136)
+        self.assertEqual(res3, 395423542667225858048)
 
     def test_large_perimeter(self):
-        res21 = triangle_perimeter(7852134593240, 481325073680324, 5329679326862463)
-        self.assertEqual(res21, 5818856535136027)
+        res4 = triangle_perimeter(7852134593240, 481325073680324, 5329679326862463)
+        self.assertEqual(res4, 5818856535136027)
 
     def test_small_perimeter(self):
-        res22 = triangle_perimeter(1, 0, 34)
-        self.assertEqual(res22, 35)
+        res5 = triangle_perimeter(1, 0, 34)
+        self.assertEqual(res5, 35)
 
 
 if __name__ == '__main__':

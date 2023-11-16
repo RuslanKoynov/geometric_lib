ae55beab44ffcbb1d33a8bbea0e342fbdd8081f0
Yandolav <144346973+Yandolav@users.noreply.github.com>
2023-11-16 12:56:32 +0300

    tests: fixed triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -21,8 +21,8 @@ class TestTriangle(unittest.TestCase):
         self.assertEqual(res, 419730)
 
     def test5_Triangle_area(self):
-        res = area(12345, -68);
-        self.assertEqual(res, False)
+        res = area(0, 0);
+        self.assertRaises(res, Exception)
 
     def test6_Triangle_area(self):
         res = area(12345.12314, 68.123412);
@@ -38,12 +38,12 @@ class TestTriangle(unittest.TestCase):
 
     def test3_Triangle_perimeter(self):
         res = perimeter(11, 12, -13)
-        self.assertEqual(res, False)
+        self.assertRaises(res, Exception)
 
     def test4_Triangle_perimeter(self):
         res = perimeter(1, 2, 123123124123)
-        self.assertEqual(res, False)
+        self.assertRaises(res, Exception)
 
-    def test4_Triangle_perimeter(self):
+    def test5_Triangle_perimeter(self):
         res = perimeter(11.12312412, 12.123124, 13.124123)
         self.assertEqual(res, 36.37037112)

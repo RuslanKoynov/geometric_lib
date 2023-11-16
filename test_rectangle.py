a55f15f0fa22ab3f872bed4b3a07e475e5b845c2
Yandolav <144346973+Yandolav@users.noreply.github.com>
2023-11-16 12:54:48 +0300

    tests: fixed rectangle

--- test_rectangle.py
+++ test_rectangle.py
@@ -9,7 +9,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(res, 20)
 
     def test2_rectangle_area(self):
-        res = area(0, 312424124123)
+        res = area(0, 15)
         self.assertEqual(res, 0)
 
     def test3_rectangle_area(self):
@@ -26,11 +26,11 @@ class TestRectangle(unittest.TestCase):
 
     def test6_rectangle_area(self):
         res = area(-10, 12)
-        self.assertEqual(res, False)
+        self.assertRaises(res, Exception)
 
     def test7_rectangle_area(self):
         res = area(120, -3)
-        self.assertEqual(res, False)
+        self.assertRaises(res, Exception)
 
     def test8_rectangle_area(self):
         res = area(120.1412424, 513.12314124)
@@ -50,17 +50,16 @@ class TestRectangle(unittest.TestCase):
 
     def test4_rectangle_perimetr(self):
         res = perimeter(-4, 1)
-        self.assertEqual(res, False)
+        self.assertRaises(res, Exception)
 
     def test5_rectangle_perimetr(self):
         res = perimeter(-12, -10)
-        self.assertEqual(res, False)
+        self.assertRaises(res, Exception)
 
     def test6_rectangle_perimetr(self):
         res = perimeter(0, 41)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, 82)
 
     def test7_rectangle_perimetr(self):
         res = perimeter(12.1233, 17.3124)
         self.assertEqual(res, 58.8714)
-

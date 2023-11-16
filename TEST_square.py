82b2ebbb4c9bd700243ffef656a6c6addd0bf4e5
Chernykh <chernykharseniy18@mail.ru>
2023-11-16 12:46:07 +0300

    test: test added for square

--- TEST_square.py
+++ TEST_square.py
@@ -14,7 +14,7 @@ class TestSquare(unittest.TestCase):
 
     def test_area_negative(self):
         res = area(-5)
-        self.assertEqual(res, "false")
+        self.assertRaises(res, Exception)
 
     def test_perimetr_normal(self):
         res = perimeter(20)
@@ -22,8 +22,8 @@ class TestSquare(unittest.TestCase):
 
     def test_perimetr_null(self):
         res = perimeter(0)
-        self.assertEqual(res, "false")
+        self.assertEqual(res, 0)
 
     def test_perimter_negative(self):
         res = perimeter(-5)
-        self.assertEqual(res, "false")
+        self.assertRaises(res, Exception)

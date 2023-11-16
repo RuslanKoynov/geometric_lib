608ebd273f9b2b883bfcb000d13faba190f17055
GreinoX <prokeg228@gmail.com>
2023-11-16 10:58:59 +0300

    test: add new tests for circle.py and functions area, perimetr

--- circle.py
+++ circle.py
@@ -1,4 +1,5 @@
 import math
+import unittest
 
 
 def area(r):
@@ -8,3 +9,43 @@ def area(r):
 def perimeter(r):
     return 2 * math.pi * r
 
+
+class CircleTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        """
+        Тестирование функции area на получение в виде аргумента числа 0
+        """
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area_multiple(self):
+        """
+        Тестирование фукнции area на получиени целочисленных различных значений
+        Округление происходит в меньшую сторону, числа после запятой отбрасываются
+        """
+        res1 = area(10)
+        res2 = area(12)
+        res3 = area(4)
+        self.assertEqual(int(res1), 314)
+        self.assertEqual(int(res2), 452)
+        self.assertEqual(int(res3), 50)
+
+
+    def test_perimetr_zero(self):
+        """
+        Тестирование функции perimetr на получение в виде аргумента числа 0
+        """
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+    
+    def test_perimetr_multiple(self):
+        """
+        Тестирование функции perimetr на получение целочисленных различных значений
+        Округление происходит в меньшую сторону, числа после запятой отбрасываются
+        """
+        res1 = perimeter(10)
+        res2 = perimeter(12)
+        res3 = perimeter(4)
+        self.assertEqual(int(res1), 62)
+        self.assertEqual(int(res2), 75)
+        self.assertEqual(int(res3), 25)

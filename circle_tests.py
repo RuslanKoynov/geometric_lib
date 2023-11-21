c0db4cbbcb401ba90435e2aef4cdf6dafa1b54a5
jjjenis <91156272+jjjenis@users.noreply.github.com>
2023-11-21 20:03:12 +0300

    edited results in zero-case in circle_tests.py

--- circle_tests.py
+++ circle_tests.py
@@ -19,7 +19,7 @@ class CircleTestCase(unittest.TestCase):
 
     def test_area_zero(self):
         res = area(0)
-        self.assertEqual(res, "Error")
+        self.assertEqual(res, 0)
 
     def test_perimeter_positive(self):
         res = perimeter(6)
@@ -35,7 +35,7 @@ class CircleTestCase(unittest.TestCase):
 
     def test_perimeter_zero(self):
         res = perimeter(0)
-        self.assertEqual(res, "Error")
+        self.assertEqual(res, 0)
 
 
 if __name__ == '__main__':

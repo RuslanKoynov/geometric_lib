a8e600cc2e09f1323b6cfc2ca09bbc990477d79f
jjjenis <deniss041272@mail.ru>
2023-11-21 12:59:35 +0300

    added rectangle.py and rectangle_tests.py to branch tests_341110

---
+++ rectangle_tests.py
@@ -0,0 +1,40 @@
+import unittest
+from rectangle import area
+from rectangle import perimeter
+
+class RectangleTestCase(unittest.TestCase):
+    def test_area_positive(self):
+        res = area(6, 7)
+        self.assertEqual(res, 42)
+
+    def test_area_negative(self):
+        res = area(10, -4)
+        self.assertEqual(res,"Error")
+
+    def test_area_real(self):
+        res = area(6.7, 7.6)
+        self.assertEqual(res, 50.92)
+
+    def test_area_zero(self):
+        res = area(0, 7)
+        self.assertEqual(res, "Error")
+
+    def test_perimeter_positive(self):
+        res = perimeter(6, 8)
+        self.assertEqual(res, 28)
+
+    def test_perimeter_negative(self):
+        res = perimeter(5, -6)
+        self.assertEqual(res, "Error")
+
+    def test_perimeter_real(self):
+        res = perimeter(6.7, 7.6)
+        self.assertEqual(res, 28.6)
+
+    def test_perimeter_zero(self):
+        res =  perimeter(5, 0)
+        self.assertEqual(res, "Error")
+
+if __name__ == "__main__":
+    unittest.main()
+

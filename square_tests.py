340b7e01866b324181b314cd36972a475c87fbef
Александр Боров <maksimka17@Marakkya.local>
2023-11-21 21:34:18 +0300

    added square tests to branch tests_409650

--- square_tests.py
+++ square_tests.py
@@ -1,39 +1,35 @@
-import unittest
 from square import area, perimeter
 import unittest
 
 class SquareTestCase(unittest.TestCase):
-    def test_zero_mul(self):
+    def test_square_area(self):
         res = area(10)
         self.assertEqual(res, 100)
 
-    def test_square_mul(self):
+    def test_square_area_2(self):
         res = area(13)
         self.assertEqual(res, 169)
 
-    def test_zero_mul(self):
+    def test_square_area_3(self):
         res = area(1)
         self.assertEqual(res, 1)
 
-    def test_square_mul(self):
+    def test_square_area_negative(self):
         res = area(-8)
         self.assertEqual(res, "ERROR")
 
-    def test_zero_mul(self):
+    def test_square_perimeter(self):
         res = perimeter(10)
         self.assertEqual(res, 40)
 
-    def test_square_mul(self):
+    def test_square_perimeter_2(self):
         res = perimeter(51)
         self.assertEqual(res, 204)
 
-    def test_zero_mul(self):
+    def test_square_perimeter_3(self):
         res = perimeter(3)
         self.assertEqual(res, 12)
 
-    def test_square_mul(self):
+    def test_square_perimeter_negative(self):
         res = perimeter(-99)
         self.assertEqual(res, "ERROR")
-
-if  __name__ == "__main__":
-    unittest.main()

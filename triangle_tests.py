4caf93b941a1ac8fc607969e3b15b729dc405007
Александр Боров <maksimka17@Marakkya.local>
2023-11-21 21:34:26 +0300

    added triangle tests to branch tests_409650

--- triangle_tests.py
+++ triangle_tests.py
@@ -2,40 +2,35 @@ from triangle import area, perimeter
 import unittest
 
 class TriangleTestCase(unittest.TestCase):
-    def test_zero_mul(self):
+    def test_triangle_area(self):
         res = area(10, 3)
         self.assertEqual(res, 15)
 
-    def test_square_mul(self):
+    def test_triangle_area_2(self):
         res = area(4, 2)
         self.assertEqual(res, 4)
 
-    def test_zero_mul(self):
+    def test_triangle_area_3(self):
         res = area(50, 10)
         self.assertEqual(res, 250)
 
-    def test_square_mul(self):
+    def test_triangle_area_negative(self):
         res = area(-9, 5)
         self.assertEqual(res, "ERROR")
 
-    def test_zero_mul(self):
+    def test_triangle_perimeter_false(self):
         res = perimeter(10, 4,5)
         self.assertEqual(res, "ERROR")
 
-    def test_square_mul(self):
+    def test_triangle_perimeter(self):
         res = perimeter(2, 4,3)
         self.assertEqual(res, 9)
 
-    def test_zero_mul(self):
+    def test_triangle_perimeter_2(self):
         res = perimeter(30, 36, 22)
         self.assertEqual(res, 88)
 
-    def test_square_mul(self):
+    def test_triangle_perimeter_negative(self):
         res = perimeter(1, 1, -1)
         self.assertEqual(res, "ERROR")
-
-if  __name__ == "__main__":
-    unittest.main()
-
-
 

8e2fbd807c51fcd46ecaa547787d8d973eaef5ea
Александр Боров <maksimka17@Marakkya.local>
2023-11-21 21:33:33 +0300

    added circle tests to branch tests_409650

--- circle_tests.py
+++ circle_tests.py
@@ -1,39 +1,33 @@
 import unittest
 from circle import area, perimeter
-import math
 class CircleTestCase(unittest.TestCase):
-    def test_area_zero(self):
+    def test_circle_area(self):
         res = area(8)
         self.assertEqual(res, 201.06192982974676)
 
-    def test_circle_mul(self):
+    def test_circle_area_negative(self):
         res = area(-1)
         self.assertEqual(res, "ERROR")
-    def test_area_zero(self):
+    def test_circle_area_2(self):
         res = area(44)
         self.assertEqual(res, 6082.123377349839)
 
-    def test_circle_mul(self):
+    def test_circle_area_null(self):
         res = area(0)
         self.assertEqual(res, 0.0)
 
-    def test_perimetr_mul(self):
-        res = area(6)
+    def test_circle_perimetr(self):
+        res = perimeter(6)
         self.assertEqual(res, 37.69911184307752)
 
-    def test_circle_mul(self):
+    def test_circle_perimetr_negative(self):
         res = perimeter(-1)
         self.assertEqual(res, "ERROR")
 
-    def test_perimetr_mul(self):
-        res = area(31)
+    def test_circle_perimetr_2(self):
+        res = perimeter(31)
         self.assertEqual(res, 194.77874452256717)
 
-    def test_circle_mul(self):
+    def test_circle_perimeter_null(self):
         res = perimeter(0)
         self.assertEqual(res, 0.0)
-
-
-if  __name__ == "__main__":
-    unittest.main()
-

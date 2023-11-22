c76372150dee99429de0212008f150d218799786
Onigiri <zaharpivnev@mail.ru>
2023-11-22 17:24:24 +0300

    Add tests for functions in square.py

--- square.py
+++ square.py
@@ -1,3 +1,29 @@
+import unittest
+class SquareTestCase(unittest.TestCase):
+    def area_test_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def area_test_number(self):
+        res = area(9)
+        self.assertEqual(res,81)
+
+    def area_test_float_number(self):
+        res = area(3.123)
+        self.assertEqual(res,9.753129000000001)
+
+    def perimeter_test_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res,0)
+
+    def perimeter_test_number(self):
+        res = perimeter(13)
+        self.assertEqual(res,52)
+
+    def perimeter_test_float_number(self):
+        res = perimeter(4.1675)
+        self.assertEqual(res,16.67)
+
 
 def area(a):
     '''
@@ -23,3 +49,5 @@ def perimeter(a):
                 4*а(int): периметр квадрата.
         '''
     return 4 * a
+
+print(perimeter(4.1675))

0b3b99534dad13a618e3f56fba8666034f2e39b8
Onigiri <zaharpivnev@mail.ru>
2023-11-22 17:22:04 +0300

    Add tests for functions in circle.py

--- circle.py
+++ circle.py
@@ -1,4 +1,31 @@
+import unittest
 import math
+
+class CircleTestCase(unittest.TestCase):
+    def area_test_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0.0)
+
+    def area_test_number(self):
+        res = area(5)
+        self.assertEqual(res, 78.53981633974483)
+
+    def area_test_float_number(self):
+        res = area(3.5)
+        self.assertEqual(res,38.48451000647496)
+
+    def perimeter_test_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res,0)
+
+    def perimeter_test_number(self):
+        res = perimeter(5)
+        self.assertEqual(res,31.41592653589793)
+
+    def perimeter_test_float_number(self):
+        res = perimeter(2.2455)
+        self.assertEqual(res,14.108892607271759)
+
 
 
 def area(r):

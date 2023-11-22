4a0956987635347e60bb8615dea13a3a775dfe80
Onigiri <zaharpivnev@mail.ru>
2023-11-22 17:22:41 +0300

    Add tests for functions in rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,31 @@
+import unittest
+
+class RectangleTestCase(unittest.TestCase):
+    def area_test_zero(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+
+    def area_test_square(self):
+        res = area(7,7)
+        self.assertEqual(res,49)
+
+    def area_test_float_number(self):
+        res = area(7.23,9.66)
+        self.assertEqual(res,69.8418)
+
+    def perimeter_test_zero(self):
+        res = perimeter(10, 0)
+        self.assertEqual(res, 0)
+
+    def perimeter_test_square(self):
+        res = perimeter(7,7)
+        self.assertEqual(res,28)
+
+    def perimeter_test_float_number(self):
+        res = perimeter(7.23,9.66)
+        self.assertEqual(res,33.78)
+
+
 def area(a,b):
     """
     Возвращает площадь прямоугольника
@@ -23,3 +51,5 @@ def perimeter(a,b):
             (a + b)*2 (int): удвоенная сумма сторон
     """
     return (a + b)*2
+
+print(perimeter(-7,8))

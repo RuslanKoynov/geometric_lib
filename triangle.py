622fea2ac1c396b902aed1bdaf548a5e2bb40fea
Onigiri <zaharpivnev@mail.ru>
2023-11-22 17:23:27 +0300

    Add tests for functions in triangle.py

--- triangle.py
+++ triangle.py
@@ -1,3 +1,38 @@
+import unittest
+
+class TriangleTestCase(unittest.TestCase):
+
+    def area_test_zero(self):
+        res = area(0,0)
+        self.assertEqual(res,0)
+
+    def area_test_number(self):
+        res = area(7,4)
+        self.assertEqual(res,14.0)
+
+    def area_test_float_number(self):
+        res = area(9.46,5.345)
+        self.assertEqual(res,25.281850000000002)
+
+    def perimeter_test_zero(self):
+        res = perimeter(0,14,5)
+        self.assertEqual(res,0)
+
+    def perimeter_test_float_number(self):
+        res = perimeter(7.4567,8.919082,6.014142)
+        self.assertEqual(res,22.389924)
+
+    def perimeter_test_number(self):
+        res = perimeter(3,4,5)
+        self.assertEqual(res,12)
+
+    def perimeter_test_number2(self):
+        res = perimeter(2,2,6)
+        self.assertEqual(res,"ERROR")
+
+
+
+
 def area(a,h):
     '''
         Возвращает площадь треугольника с основанием a и высотой h.
@@ -24,3 +59,5 @@ def perimeter(a,b,c):
                 a + и + с: периметр треугольника
     '''
     return a + b + c
+
+print(perimeter(7.4567,8.919082,6.014142))

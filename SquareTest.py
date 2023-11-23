1669a9423b646006d5bf3b115b2325feff85c777
MisterMLG <76111947+MisterMLG@users.noreply.github.com>
2023-11-23 13:12:13 +0300

    Update SquareTest.py

--- SquareTest.py
+++ SquareTest.py
@@ -16,9 +16,9 @@ class SquareTest(unittest.TestCase):
         result = area(0)
         self.assertEqual(result, 0)
 
-    def test_for_area_zero(self):
-        result = area(-3)
-        self.assertEqual(result, "Error")
+    def test_string_area(self):
+        with self.assertRaises(TypeError):
+            area('0')
 
     #test for perimeter
     def test_for_perimeter_first_integer(self):
@@ -33,6 +33,6 @@ class SquareTest(unittest.TestCase):
         result = perimeter(0)
         self.assertEqual(result, 0)
         
-    def test_for_perimeter_zero(self):
-        result = perimeter(0)
-        self.assertEqual(result, "Error")
+    def test_string_perim(self):
+        with self.assertRaises(TypeError):
+            perimeter('0')

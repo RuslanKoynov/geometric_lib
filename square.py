aa37ad0fd8a3872a71117a81df34337760fbbece
Floym7 <floym7@mail.ru>
2023-11-25 18:50:04 +0300

    refactor: calculation logic with integrated exception handling

--- square.py
+++ square.py
@@ -1,7 +1,21 @@
-
 def area(a):
+    """
+    Принимает число a(сторона квадрата), возвращает площадь квадрата
+    """
+    if (not(isinstance(a, int))):
+        raise TypeError("Invalid data type")
+    if (a < 0):
+        raise ValueError("Invalid value")
     return a * a
 
 
 def perimeter(a):
+    """
+    Принимает число a(сторона квадрата), возвращает периметр квадрата
+    """
+    if (not(isinstance(a, int))):
+        raise TypeError("Invalid data type")
+    if (a < 0):
+        raise ValueError("Invalid value")
     return 4 * a
+    

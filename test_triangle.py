d0600e9b8743a60556d3a020d142cd07218822cc
Yandolav <144346973+Yandolav@users.noreply.github.com>
2023-11-28 17:01:59 +0300

    test: Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -31,7 +31,3 @@ class TestTriangle(unittest.TestCase):
     def test2_Triangle_perimeter(self):
         res = perimeter(11, 12, 13)
         self.assertEqual(res, 36)
-
-    def test5_Triangle_perimeter(self):
-        res = perimeter(11.12312412, 12.123124, 13.124123)
-        self.assertEqual(res, 36.37037112)

9b24da72a270088ba17beef456380a277fb221ee
ChernykhITMO <144235932+ChernykhITMO@users.noreply.github.com>
2023-11-28 16:08:31 +0300

    Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -10,9 +10,6 @@ class TestCircle(unittest.TestCase):
     def test_circle_area_normal(self):
         res = area(1)
         self.assertEqual(res, 3.141592653589793)
-    def test_area_negative(self):
-        res = area(-5)
-        self.assertRaises(res, 0)
 
     def test_perimetr_null(self):
         res = perimeter(0)
@@ -22,6 +19,3 @@ class TestCircle(unittest.TestCase):
         res = perimeter(5)
         self.assertEqual(res, 31.41592653589793)
 
-    def test_large_perimetr_negative(self):
-        res = perimeter(-15)
-        self.assertRaises(res, 0)

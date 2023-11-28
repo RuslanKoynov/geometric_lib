a26d24c3b8e3a5afb2e13caa6247f1059001d458
ChernykhITMO <144235932+ChernykhITMO@users.noreply.github.com>
2023-11-28 16:08:08 +0300

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -12,10 +12,6 @@ class TestSquare(unittest.TestCase):
         res = area(0)
         self.assertEqual(res, 0)
 
-    def test_area_negative(self):
-        res = area(-5)
-        self.assertRaises(res, Exception)
-
     def test_perimetr_normal(self):
         res = perimeter(20)
         self.assertEqual(res, 80)
@@ -23,7 +19,3 @@ class TestSquare(unittest.TestCase):
     def test_perimetr_null(self):
         res = perimeter(0)
         self.assertEqual(res, 0)
-
-    def test_perimter_negative(self):
-        res = perimeter(-5)
-        self.assertRaises(res, Exception)

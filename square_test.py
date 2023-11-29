45216748df3f48f8df07cc925335d4f61372e0e3
Levman228 <levkuimov34@gmail.com>
2023-11-29 14:56:55 +0300

    Added tests

---
+++ square_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from square import area
+from square import perimeter
+
+class MyTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area_small_input(self):
+        res = area(3)
+        self.assertEqual(res, 9)
+
+    def test_area_big_input(self):
+        res = area(19123872193782)
+        self.assertEqual(res, 19123872193782 * 19123872193782)
+
+    def test_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_perimeter_small_input(self):
+        res = perimeter(3)
+        self.assertEqual(res, 12)
+
+    def test_perimeter_big_input(self):
+        res = perimeter(19123872193782)
+        self.assertEqual(res, 19123872193782 * 19123872193782)

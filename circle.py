8583203d67bbcd741091617e8c3a092825cdff36
alekshi-web <144231617+alekshi-web@users.noreply.github.com>
2023-11-30 01:52:15 +0300

    circle.py

--- circle.py
+++ circle.py
@@ -29,7 +29,7 @@ actual_perimeter = perimeter(r)
 print(f"Тест 2: r = {r}")
 print(f"Ожидаемая площадь: {expected_area}, Фактическая площадь: {actual_area}")
 print(f"Ожидаемый периметр: {expected_perimeter}, Фактический периметр: {actual_perimeter}")
-print("error")
+print()
 
 # Тест 3: Проверка площади и периметра круга с нулевым радиусом
 r = 0
@@ -42,26 +42,41 @@ print(f"Ожидаемая площадь: {expected_area}, Фактическа
 print(f"Ожидаемый периметр: {expected_perimeter}, Фактический периметр: {actual_perimeter}")
 print()
 
-class CircleTests(unittest.TestCase):
-    def test_area(self):
-        # Проверяем правильность вычисления площади круга
-        self.assertEqual(area(2), math.pi * 2 * 2)  # Входные данные: r = 2; Ожидаемый результат: pi * 2 * 2; Фактический результат: pi * 2 * 2
-        self.assertEqual(area(4), math.pi * 4 * 4)  # Входные данные: r = 4; Ожидаемый результат: pi * 4 * 4; Фактический результат: pi * 4 * 4
-        self.assertEqual(area(0), math.pi * 0 * 0)  # Входные данные: r = 0; Ожидаемый результат: pi * 0 * 0; Фактический результат: pi * 0 * 0
+import unittest
+from circle import area,perimeter
 
-    def test_perimeter(self):
-        # Проверяем правильность вычисления длины окружности
-        self.assertEqual(perimeter(2), 2 * math.pi * 2)  # Входные данные: r = 2; Ожидаемый результат: 2 * pi * 2; Фактический результат: 2 * pi * 2
-        self.assertEqual(perimeter(4), 2 * math.pi * 4)  # Входные данные: r = 4; Ожидаемый результат: 2 * pi * 4; Фактический результат: 2 * pi * 4
-        self.assertEqual(perimeter(0), 2 * math.pi * 0)  # Входные данные: r = 0; Ожидаемый результат: 2 * pi * 0; Фактический результат: 2 * pi * 0
+class CircleTestCase(unittest.TestCase):
+    def test_circle_area_yes(self):
+        res = area(4)
+        self.assertEqual(res, 50.26548245743669)
 
-    def test_area_with_large_number(self):
-        # Проверяем правильность вычисления площади круга с очень большим числом
-        self.assertEqual(area(1000000), math.pi * 1000000 * 1000000)  # Входные данные: r = 1000000; Ожидаемый результат: pi * 1000000 * 1000000; Фактический результат: pi * 1000000 * 1000000
+    def test_circle_area_no(self):
+        res = area(-4)
+        self.assertEqual(res, 'Radius cannot be negative')
 
-    def test_perimeter_with_large_number(self):
-        # Проверяем правильность вычисления длины окружности с очень большим числом
-        self.assertEqual(perimeter(1000000), 2 * math.pi * 1000000)  # Входные данные: r = 1000000; Ожидаемый результат: 2 * pi * 1000000; Фактический результат: 2 * pi * 1000000
+    def test_circle_area_real_numbers(self):
+        res = area(4.25)
+        self.assertEqual(res, 56.745017305465645)
+
+    def test_circle_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_circle_perimeter_yes(self):
+        res = perimeter(4)
+        self.assertEqual(res, 25.132741228718345)
+
+    def test_circle_perimeter_no(self):
+        res = perimeter(-4)
+        self.assertEqual(res, 'Radius cannot be negative')
+
+    def test_circle_perimeter_real_numbers(self):
+        res = perimeter(4.25)
+        self.assertEqual(res, 26.703537555513243)
+
+    def test_circle_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
 
 if __name__ == '__main__':
     unittest.main()

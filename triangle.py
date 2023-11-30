82ced6e3e23b1fba05662c8a23d5aceb656b06d8
alekshi-web <144231617+alekshi-web@users.noreply.github.com>
2023-11-30 11:04:29 +0300

    triangle.py

--- triangle.py
+++ triangle.py
@@ -32,7 +32,7 @@ actual_perimeter = perimeter(a, b, c)
 print(f"Тест 2: a = {a}, h = {h}, b = {b}, c = {c}")
 print(f"Ожидаемая площадь: {expected_area}, Фактическая площадь: {actual_area}")
 print(f"Ожидаемый периметр: {expected_perimeter}, Фактический периметр: {actual_perimeter}")
-print("error")
+print()
 
 # Тест 3: Проверка площади и периметра треугольника с нулевыми значениями основания и высоты
 a = 0
@@ -48,26 +48,39 @@ print(f"Ожидаемая площадь: {expected_area}, Фактическа
 print(f"Ожидаемый периметр: {expected_perimeter}, Фактический периметр: {actual_perimeter}")
 print()
 
-class TriangleTests(unittest.TestCase):
-    def test_area(self):
-        # Проверяем правильность вычисления площади треугольника
-        self.assertEqual(area(2, 3), 3)  # Входные данные: a = 2, h = 3; Ожидаемый результат: 3; Фактический результат: 3
-        self.assertEqual(area(4, 5), 10)  # Входные данные: a = 4, h = 5; Ожидаемый результат: 10; Фактический результат: 10
-        self.assertEqual(area(0, 0), 0)  # Входные данные: a = 0, h = 0; Ожидаемый результат: 0; Фактический результат: 0
+from triangle import area, perimeter
+class TriangleTestCase(unittest.TestCase):
+    def test_triangle_area_yes(self):
+        res = area(2, 8)
+        self.assertEqual(res, 8)
 
-    def test_perimeter(self):
-        # Проверяем правильность вычисления периметра треугольника
-        self.assertEqual(perimeter(2, 3, 4), 9)  # Входные данные: a = 2, b = 3, c = 4; Ожидаемый результат: 9; Фактический результат: 9
-        self.assertEqual(perimeter(4, 5, 6), 15)  # Входные данные: a = 4, b = 5, c = 6; Ожидаемый результат: 15; Фактический результат: 15
-        self.assertEqual(perimeter(0, 0, 0), 0)  # Входные данные: a = 0, b = 0, c = 0; Ожидаемый результат: 0; Фактический результат: 0
+    def test_triangle_area_no(self):
+        res = area(2, -8)
+        self.assertEqual(res, 'Base and height cannot be negative')
 
-    def test_area_with_large_number(self):
-        # Проверяем правильность вычисления площади треугольника с очень большим числом
-        self.assertEqual(area(1000000, 1000000), 500000000000)  # Входные данные: a = 1000000, h = 1000000; Ожидаемый результат: 500000000000; Фактический результат: 500000000000
+    def test_triangle_area_real_numbers(self):
+        res = area(2.75, 8.6)
+        self.assertEqual(res, 11.825)
 
-    def test_perimeter_with_large_number(self):
-        # Проверяем правильность вычисления периметра треугольника с очень большим числом
-        self.assertEqual(perimeter(1000000, 1000000, 1000000), 3000000)  # Входные данные: a = 1000000, b = 1000000, c = 1000000; Ожидаемый результат: 3000000; Фактический результат: 3000000
+    def test_triangle_area_zero(self):
+        res = area(2, 0)
+        self.assertEqual(res, 0)
+
+    def test_triangle_perimeter_yes(self):
+        res = perimeter(2, 8, 11)
+        self.assertEqual(res, 21)
+
+    def test_triangle_perimeter_no(self):
+        res = perimeter(2, -8, 11)
+        self.assertEqual(res, 'Base and height cannot be negative')
+
+    def test_triangle_perimeter_real_numbers(self):
+        res = perimeter(2.75, 8.6, 11)
+        self.assertEqual(res, 22.35)
+
+    def test_triangle_perimeter_zero(self):
+        res = perimeter(2, 0, 8.59)
+        self.assertEqual(res, 10.59)
 
 if __name__ == '__main__':
     unittest.main()

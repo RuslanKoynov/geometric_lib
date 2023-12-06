b60094ff059cd045ad8dcaf91a41c8cfe5cb0a2e
Александр Боров <maksimka17@Marakkya.local>
2023-12-06 15:24:57 +0300

    added tests to rectangle

--- rectangle_tests.py
+++ rectangle_tests.py
@@ -16,7 +16,7 @@ class RectangleTestCase(unittest.TestCase):
 
     def test_rectangle_area_float(self):
         res = area(90.22, 1.5)
-        self.assertAlmostEqual(res, 135.32, places=2)
+        self.assertAlmostEqual(res, 135.33, places=2)
 
     def test_rectangle_perimeter_integer(self):
         res = perimeter(10, 1)
@@ -24,7 +24,7 @@ class RectangleTestCase(unittest.TestCase):
 
     def test_rectangle_perimeter_zero(self):
         res = perimeter(0, 20)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, 40)
 
     def test_rectangle_perimeter_float(self):
         res = perimeter(2.1,5.5)

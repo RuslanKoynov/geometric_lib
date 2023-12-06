c1bea7c3b4e8af87515cf5161b791600d6be3263
Александр Боров <maksimka17@Marakkya.local>
2023-12-06 15:25:19 +0300

    added tests to square

--- square_tests.py
+++ square_tests.py
@@ -2,33 +2,32 @@ from square import area, perimeter
 import unittest
 
 class SquareTestCase(unittest.TestCase):
-    def test_square_area(self):
+    def test_square_area_integer(self):
         res = area(10)
         self.assertEqual(res, 100)
 
-    def test_square_area_2(self):
-        res = area(13)
-        self.assertEqual(res, 169)
+    def test_square_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
 
-    def test_square_area_3(self):
-        res = area(1)
-        self.assertEqual(res, 1)
-
+    def test_square_area_float(self):
+        res = area(8.69)
+        self.assertAlmostEqual(res, 75.51, places=1)
     def test_square_area_negative(self):
         res = area(-8)
         self.assertEqual(res, "ERROR")
 
     def test_square_perimeter(self):
-        res = perimeter(10)
-        self.assertEqual(res, 40)
+        res = perimeter(11)
+        self.assertEqual(res, 44)
 
-    def test_square_perimeter_2(self):
-        res = perimeter(51)
-        self.assertEqual(res, 204)
+    def test_square_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
 
-    def test_square_perimeter_3(self):
-        res = perimeter(3)
-        self.assertEqual(res, 12)
+    def test_square_perimeter_float(self):
+        res = perimeter(3.33)
+        self.assertEqual(res, 13.32)
 
     def test_square_perimeter_negative(self):
         res = perimeter(-99)

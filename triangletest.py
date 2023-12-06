dfb267a2ca09b7309c44ed7314521dbe01de6724
davidushka26 <107818269+davidushka26@users.noreply.github.com>
2023-12-06 16:57:40 +0300

    Function tests have been  changed

--- triangletest.py
+++ triangletest.py
@@ -3,25 +3,25 @@ import triangle
 
 class TestTriangleCase(unittest.TestCase):
     def test_positive_area(self):
-        res = triangle.area(12, 6)
-        self.assertEqual(res, 36)
+        res = triangle.area(20, 6)
+        self.assertEqual(res, 60)
 
     def test_zero_area(self):
-        res = triangle.area(0, 5)
-        self.assertEqual(res, 0)
+        with self.assertRaises(ValueError):
+            res = triangle.area(0)
 
-    def test_negative_area(self):
-        res = area(4, -5)
-        self.assertEqual(res, 0)
+    def test_negative_side_triangle_area(self):
+        with self.assertRaises(ValueError):
+            res = triangle.area(0)
 
     def test_positive_perimeter(self):
-        res = triangle.perimeter(15, 5, 8)
-        self.assertEqual(res, 28)
+        res = triangle.perimeter(10, 5, 15)
+        self.assertEqual(res, 30)
 
     def test_zero_perimetr(self):
-        res = triangle.perimeter(5, 9, 0)
-        self.assertEqual(res, 0)
+        with self.assertRaises(ValueError):
+            res = triangle.perimetr(0)
 
-    def test_negative_perimetr(self):
-        res = perimeter(4, 9, -6)
-        self.assertEqual(res, 0)
+    def test_negative_side_triangle_perimetr(self):
+        with self.assertRaises(ValueError):
+            res = triangle.perimetr(0)

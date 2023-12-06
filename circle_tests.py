d52b58285ea503a95e985a7def75355670de62df
Александр Боров <maksimka17@Marakkya.local>
2023-12-06 15:24:31 +0300

    added tests to circle

--- circle_tests.py
+++ circle_tests.py
@@ -8,10 +8,10 @@ class CircleTestCase(unittest.TestCase):
 
     def test_circle_area_negative(self):
         res = area(-1)
-        self.assertEqual(res, "ERROR")
+        self.assertAlmostEqual(res, "ERROR")
     def test_circle_area_integer(self):
         res = area(10)
-        self.assertAlmostEqual(res, 314.15, places=2)
+        self.assertAlmostEqual(res, 314.16, places=2)
 
     def test_circle_area_zero(self):
         res = area(0)
@@ -19,18 +19,18 @@ class CircleTestCase(unittest.TestCase):
 
     def test_circle_perimetr_integer(self):
         res = perimeter(6)
-        self.assertAlmostEqual(res, 37.69, places=2)
+        self.assertAlmostEqual(res, 37.7, places=1)
 
     def test_circle_perimetr_negative(self):
         res = perimeter(-1)
-        self.assertEqual(res, "ERROR")
+        self.assertAlmostEqual(res, "ERROR")
 
     def test_circle_perimetr_float(self):
         res = perimeter(2.88)
-        self.assertAlmostEqual(res, 18.09, places=2)
+        self.assertAlmostEqual(res, 18.1, places=1)
 
     def test_circle_perimeter_zero(self):
         res = perimeter(0)
-        self.assertEqual(res, 0)
+        self.assertAlmostEqual(res, 0)
 
 

0e0fa10b188b9de37cb4fac83f2938709afd8781
davidushka26 <107818269+davidushka26@users.noreply.github.com>
2023-12-06 16:55:32 +0300

    Function tests have been changed

--- rectangletest.py
+++ rectangletest.py
@@ -3,25 +3,25 @@ import rectangle
 
 class RectangleTestCase(unittest.TestCase):
     def test_zero_area(self):
-        res = rectangle.area(10, 0)
-        self.assertEqual(res, 0)
-
-    def test_square_area(self):
         res = rectangle.area(10, 10)
         self.assertEqual(res, 100)
 
+    def test_positive_area(self):
+        with self.assertRaises(ValueError):
+            res = rectangle.area(0)
+
     def test_negative_side_rectangle_area(self):
-        res = area(5, -5)
-        self.assertEqual(res, 0)
+        with self.assertRaises(ValueError):
+            res = rectangle.area(0)
 
     def test_zero_perimeter(self):
-        res = rectangle.perimeter(0, 5)
-        self.assertEqual(res, 0)
+        with self.assertRaises(ValueError):
+            res = rectangle.perimetr(0)
 
     def test_square_perimeter(self):
-        res = rectangle.perimeter(20, 20)
-        self.assertEqual(res, 80)
+        res = rectangle.perimeter(20, 10)
+        self.assertEqual(res, 60)
 
     def test_negative_side_rectangle_perimetr(self):
-        res = perimeter(5, -5)
-        self.assertEqual(res, 0)
+        with self.assertRaises(ValueError):
+            res = rectangle.area(0)

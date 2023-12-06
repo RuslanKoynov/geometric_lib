d1f06333fc262c88936ff21df8e97f523e8aa449
Александр Боров <maksimka17@Marakkya.local>
2023-12-06 15:25:42 +0300

    added tests to triangle

--- triangle_tests.py
+++ triangle_tests.py
@@ -2,33 +2,37 @@ from triangle import area, perimeter
 import unittest
 
 class TriangleTestCase(unittest.TestCase):
-    def test_triangle_area(self):
+    def test_triangle_area_integer(self):
         res = area(10, 3)
-        self.assertEqual(res, 15)
+        self.assertAlmostEqual(res, 15)
 
-    def test_triangle_area_2(self):
-        res = area(4, 2)
-        self.assertEqual(res, 4)
+    def test_triangle_area_zero(self):
+        res = area(4, 0)
+        self.assertEqual(res, 0)
 
-    def test_triangle_area_3(self):
-        res = area(50, 10)
-        self.assertEqual(res, 250)
+    def test_triangle_area_float(self):
+        res = area(2.2, 10.9)
+        self.assertAlmostEqual(res, 11.99)
 
     def test_triangle_area_negative(self):
         res = area(-9, 5)
         self.assertEqual(res, "ERROR")
 
+    def test_triangle_perimetr_zero(self):
+        res = perimeter(0, 2, 1)
+        self.assertAlmostEqual(res, 3)
+
     def test_triangle_perimeter_false(self):
         res = perimeter(10, 4,5)
         self.assertEqual(res, "ERROR")
 
-    def test_triangle_perimeter(self):
+    def test_triangle_perimeter_integer(self):
         res = perimeter(2, 4,3)
         self.assertEqual(res, 9)
 
-    def test_triangle_perimeter_2(self):
-        res = perimeter(30, 36, 22)
-        self.assertEqual(res, 88)
+    def test_triangle_perimeter_float(self):
+        res = perimeter(3.2, 3.9, 4.5)
+        self.assertEqual(res, 11.6)
 
     def test_triangle_perimeter_negative(self):
         res = perimeter(1, 1, -1)

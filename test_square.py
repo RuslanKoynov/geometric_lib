7dbe2f8a67991adf633c97f26f76af2ef3c607ef
Adriana Khudoba <144237605+AdrianaKhudoba@users.noreply.github.com>
2023-12-07 02:13:00 +0300

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -6,10 +6,6 @@ class SquareTestCase(unittest.TestCase):
         res = area(3)
         self.assertEqual(res, 9)
 
-    def test_square_area_no(self):
-        res = area(-3)
-        self.assertEqual(res, 'The side of a square cannot be negative')
-
     def test_square_area_real_numbers(self):
         res = area(3.45)
         self.assertEqual(res, 11.902500000000002)
@@ -21,10 +17,6 @@ class SquareTestCase(unittest.TestCase):
     def test_square_perimeter_yes(self):
         res = perimeter(3)
         self.assertEqual(res, 12)
-
-    def test_square_perimeter_no(self):
-        res = perimeter(-3)
-        self.assertEqual(res,'The side of a square cannot be negative')
 
     def test_square_perimeter_real_numbers(self):
         res = perimeter(3.45)

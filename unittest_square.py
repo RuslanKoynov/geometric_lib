c43bf12fb058065f7bd7360e6cc1aa7e71134047
Mogohars <Qweqweqwe1232005@gmail.com>
2023-12-13 16:19:42 +0300

    test: unittests was uploaded

---
+++ unittest_square.py
@@ -0,0 +1,37 @@
+import unittest
+
+from square import area
+from square import perimeter
+
+class squareTest(unittest.TestCase):
+    def test_square_area_1(self):
+        res = area(5)
+        self.assertEqual(res, 25)
+
+    def test_square_area_2(self):
+        res = area(-5)
+        self.assertEqual(res, 0)
+
+    def test_square_area_3(self):
+        res = area("5")
+        self.assertEqual(res, 25)
+
+    def test_square_area_4(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_square_perimeter_1(self):
+        res = perimeter(5)
+        self.assertEqual(res,20)
+
+    def test_square_perimeter_2(self):
+        res = perimeter(-5)
+        self.assertEqual(res, 0)
+
+    def test_square_perimeter_3(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_square_perimeter_4(self):
+        res = perimeter("5")
+        self.assertEqual(res, 20)

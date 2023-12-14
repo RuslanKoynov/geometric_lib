edf0db66db105b09881f0e7279b339ef16e3c884
nxrmxlnx <144320217+nxrmxlnx@users.noreply.github.com>
2023-12-14 16:20:44 +0700

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -5,15 +5,20 @@ from square import perimeter
 
 class TestSquareArea(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(area(0), 0**2)
-        self.assertEqual(area(1), 1**2)
-        self.assertEqual(area(3), 3**2)
-        self.assertEqual(area(35616533), 35616533**2)
+        self.assertAlmostEqual(area(0), 0)
+    def test_area(self):
+        self.assertAlmostEqual(area(1), 1)
+    def test_area(self):
+        self.assertAlmostEqual(area(3), 9)
+    def test_area(self):
+        self.assertAlmostEqual(area(35616533), 1.26854e15)
 
 class TestSquarePerimeter(unittest.TestCase):
     def test_perimeter(self):
-        self.assertEqual(perimeter(0), 4*0)
-        self.assertEqual(perimeter(1), 4*1)
-        self.assertEqual(perimeter(3), 4*3)
-        self.assertEqual(perimeter(35616533), 4*35616533)
-
+        self.assertAlmostEqual(perimeter(0), 0)
+    def test_perimeter(self):
+        self.assertvEqual(perimeter(1), 4)
+    def test_perimeter(self):
+        self.assertAlmostEqual(perimeter(3), 12)
+    def test_perimeter(self):
+        self.assertAlmostEqual(perimeter(35616533), 142466132)

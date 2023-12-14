b8519107713002112dfc835ad5ad69e8da3b1c54
nxrmxlnx <144320217+nxrmxlnx@users.noreply.github.com>
2023-12-14 15:08:49 +0700

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -6,14 +6,20 @@ from triangle import perimeter
 class TestTriangleArea(unittest.TestCase):
     def test_area(self):
         self.assertEqual(area(0,1), 0*1/2)
+    def test_area(self):
         self.assertEqual(area(1,2), 1*2/2)
+    def test_area(self):
         self.assertEqual(area(3,4), 3*4/2)
+    def test_area(self):
         self.assertEqual(area(35616533,83274987), 35616533*83274987/2)
 
 class TestTrianglePerimeter(unittest.TestCase):
     def test_perimeter(self):
         self.assertEqual(perimeter(0,1,2),0+1+2)
+    def test_perimeter(self):
         self.assertEqual(perimeter(1,2,3), 1+2+3)
+    def test_perimeter(self):
         self.assertEqual(perimeter(3,4,5), 3+4+5)
+    def test_perimeter(self):
         self.assertEqual(perimeter(35616533,83274987,98473),35616533+83274987+98473)
 

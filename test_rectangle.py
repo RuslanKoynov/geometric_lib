3b49302a40502fb403dc99a72be12f3f0c48acde
nxrmxlnx <144320217+nxrmxlnx@users.noreply.github.com>
2023-12-14 16:15:58 +0700

    Update test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -5,21 +5,21 @@ from rectangle import perimeter
 
 class TestRectangleArea(unittest.TestCase):
     def test_area(self):
-        self.assertAlmostEqual(area(0,1), 0*1)
+        self.assertAlmostEqual(area(0,1), 0)
     def test_area(self):
-        self.assertAlmostEqual(area(1,2), 1*2)
+        self.assertAlmostEqual(area(1,2), 2)
     def test_area(self):
-        self.assertAlmostEqual(area(3,4), 3*4)
+        self.assertAlmostEqual(area(3,4), 12)
     def test_area(self):
-        self.assertAlmostEqual(area(35616533,38749), 35616533*38749)
+        self.assertAlmostEqual(area(35616533,38749), 4.91546e19)
 
 class TestRectanglePerimeter(unittest.TestCase):
     def test_perimeter(self):
-        self.assertAlmostEqual(perimeter(0, 1), 2*(0 + 1))
+        self.assertAlmostEqual(perimeter(0, 1), 2)
     def test_perimeter(self):
-        self.assertAlmostEqual(perimeter(1, 2), 2*(1 + 2))
+        self.assertAlmostEqual(perimeter(1, 2), 6)
     def test_perimeter(self):
-        self.assertAlmostEqual(perimeter(3, 4), 2*(3 + 4))
+        self.assertAlmostEqual(perimeter(3, 4), 14)
     def test_perimeter(self):
-        self.assertAlmostEqual(perimeter(35616533, 38749), 2*(35616533 + 38749))
+        self.assertAlmostEqual(perimeter(35616533, 38749), 71310564)
 

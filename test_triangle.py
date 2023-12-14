941b707c7613ce12a856f8f22f4521d3ac40ff4e
nxrmxlnx <144320217+nxrmxlnx@users.noreply.github.com>
2023-12-14 15:35:57 +0700

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -5,21 +5,21 @@ from triangle import perimeter
 
 class TestTriangleArea(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(area(0,1), 0*1/2)
+        self.assertAlmostEqual(area(0,1), 0*1/2)
     def test_area(self):
-        self.assertEqual(area(1,2), 1*2/2)
+        self.assertAlmostEqual(area(1,2), 1*2/2)
     def test_area(self):
-        self.assertEqual(area(3,4), 3*4/2)
+        self.assertAlmostEqual(area(3,4), 3*4/2)
     def test_area(self):
-        self.assertEqual(area(35616533,83274987), 35616533*83274987/2)
+        self.assertAlmostEqual(area(35616533,83274987), 35616533*83274987/2)
 
 class TestTrianglePerimeter(unittest.TestCase):
     def test_perimeter(self):
-        self.assertEqual(perimeter(0,1,2),0+1+2)
+        self.assertAlmostEqual(perimeter(0,1,2),0+1+2)
     def test_perimeter(self):
-        self.assertEqual(perimeter(1,2,3), 1+2+3)
+        self.assertAlmostEqual(perimeter(1,2,3), 1+2+3)
     def test_perimeter(self):
-        self.assertEqual(perimeter(3,4,5), 3+4+5)
+        self.assertAlmostEqual(perimeter(3,4,5), 3+4+5)
     def test_perimeter(self):
-        self.assertEqual(perimeter(35616533,83274987,98473),35616533+83274987+98473)
+        self.assertAlmostEqual(perimeter(35616533,83274987,98473),35616533+83274987+98473)
 

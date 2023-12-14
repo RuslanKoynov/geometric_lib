509ead770f238eadf64f484ec0ffe4849d3942fa
nxrmxlnx <144320217+nxrmxlnx@users.noreply.github.com>
2023-12-14 17:48:51 +0700

    Update test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -9,7 +9,7 @@ class TestRectangleArea(unittest.TestCase):
     def test_area(self):
         self.assertAlmostEqual(area(1,2), 2)
     def test_area(self):
-        self.assertAlmostEqual(area(3,4), 12)
+        self.assertAlmostEqual(area(-3,-4), error)
     def test_area(self):
         self.assertAlmostEqual(area(35616533,38749), 4.91546e19)
 

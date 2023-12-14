66f4566a8397e71a26b39c4c26e194625552dd20
nxrmxlnx <144320217+nxrmxlnx@users.noreply.github.com>
2023-12-14 18:23:16 +0700

    Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -7,11 +7,11 @@ class TestCircleArea(unittest.TestCase):
     def test_area(self):
         self.assertAlmostEqual(area(0), 0)
     def test_area(self):
-        self.assertAlmostEqual(round(area(1),2), 3.14)
+        self.assertAlmostEqual(area(1), 3.14)
     def test_area(self):
-        self.assertAlmostEqual(area(3), 28.26)
+        self.assertRaises(ValueError,area, -3)
     def test_area(self):
-        self.assertAlmostEqual(area(35616533), 3.98321e15)
+        self.assertAlmostEqual(area(35616533), 3983207508031880)
 
 class TestCirclePerimeter(unittest.TestCase):
     def test_perimeter(self):

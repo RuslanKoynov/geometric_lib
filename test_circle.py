40ff8b46089900e1451842009d38af08dc5f7ba7
nxrmxlnx <144320217+nxrmxlnx@users.noreply.github.com>
2023-12-14 15:06:52 +0700

    Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -6,14 +6,20 @@ from math import pi
 class TestCircleArea(unittest.TestCase):
     def test_area(self):
         self.assertEqual(area(0), pi * 0 ** 2)
+    def test_area(self):
         self.assertEqual(area(1), pi * 1 ** 2)
+    def test_area(self):
         self.assertEqual(area(3), pi * 3 ** 2)
+    def test_area(self):
         self.assertEqual(area(35616533), pi*35616533**2)
 
 class TestCirclePerimeter(unittest.TestCase):
     def test_perimeter(self):
         self.assertEqual(perimeter(0), pi * 0 * 2)
+    def test_perimeter(self):
         self.assertEqual(perimeter(1), pi * 1 * 2)
+    def test_perimeter(self):
         self.assertEqual(perimeter(3), pi * 3 * 2)
+    def test_perimeter(self):
         self.assertEqual(perimeter(35616533), pi * 35616533 * 2)
 

020d5d11d42578b128dabe69b102157a497b01c3
Ivannka <gertazx@icloud.com>
2023-12-21 01:28:04 +0300

    test:add new tests fo circle.py

--- circle.py
+++ circle.py
@@ -1,15 +1,46 @@
-import math
+import unittest
+
 
 
 def area(r):
     '''
     Принимает число r(радиус круга),возвращает площадь круга с этим радиусом
     '''
-    return math.pi * r * r
+    return 3.14 * r * r
 
 def perimeter(r):
     '''
     Принимает число r(радиус круга),возвращает периметр круга с этим радиусом
-    '''git
-    return 2 * math.pi * r
+    '''
+    return 2 * 3.14 * r
 
+
+class CircleTest(unittest.TestCase):
+
+    def test_area1(self):
+        res = area(5)
+        self.assertEqual(res, 78.5)
+
+    def test_perimeter1(self):
+        res = perimeter(5)
+        self.assertEqual(res, 31.4)
+
+    def test_area2(self):
+        res = area(4.5)
+        self.assertEqual(res, 63.585)
+
+    def test_perimeter2(self):
+        res = perimeter(4.5)
+        self.assertEqual(res, 28.26)
+
+    def test_area3(self):
+        with self.assertRaises(Exception):
+            area(-1,-20)
+
+    def test_perimeter3(self):
+        with self.assertRaises(Exception):
+            perimeter(-3,-60)
+
+
+if __name__ == "__main__":
+    unittest.ma

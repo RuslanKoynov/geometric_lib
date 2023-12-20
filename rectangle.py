08da846b36db3014c108e2c36a7aac91472839f0
Ivannka <gertazx@icloud.com>
2023-12-21 01:29:34 +0300

    test: add new tests for rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,5 @@
-def area(a, b): 
+import unittest
+def area(a, b):
     '''
     Возвращает площадь прямоугольника
 
@@ -17,3 +18,34 @@ def perimeter(a, b):
            b (int):значение второй стороны прямоугольника
     '''
     return 2*(a + b)
+
+class RectangleTest(unittest.TestCase):
+
+    def test_area1(self):
+        res = area(1,2)
+        self.assertEqual(res, 2)
+
+    def test_perimeter1(self):
+        res = perimeter(5,5)
+        self.assertEqual(res, 20)
+
+    def test_area2(self):
+        res = area(4.5,6.5)
+        self.assertEqual(res, 29.25)
+
+    def test_perimeter2(self):
+        res = perimeter(4.5,6.5)
+        self.assertEqual(res, 22)
+
+    def test_area3(self):
+        with self.assertRaises(Exception):
+            area(-10,-5)
+
+    def test_perimeter3(self):
+        with self.assertRaises(Exception):
+            perimeter(-5,-10)
+
+
+if __name__ == "__main__":
+    unittest.ma
+

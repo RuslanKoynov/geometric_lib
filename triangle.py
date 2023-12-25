7e9986fd43c50470d66c322ae4b4841725053378
Руслан Самигуллин <144239367+rssolgaleo@users.noreply.github.com>
2023-12-25 16:24:44 +0300

    tests: code triangle.py is changed

--- triangle.py
+++ triangle.py
@@ -1,5 +1,42 @@
-def area(a, h): 
-    return a * h / 2 
+import unittest
 
-def perimeter(a, b, c): 
+''' Принимает числа a, h и возвращает площадь треугольника '''
+def area(a, h):
+    return a * h / 2
+''' Принимает числа a, b, c и возвращает периметр треугольника '''
+def perimeter(a, b, c):
     return a + b + c 
+
+class TriangleTestCase(unittest.TestCase):
+
+   def test_area_1(self):
+       res = area(4, 5)
+       self.assertAlmostEqual(res, 10, delta=0.1)
+   def test_perimeter_1(self):
+       res = perimeter(4, 5, 6)
+       self.assertAlmostEqual(res, 15, delta=0.1)
+
+   def test_area_2(self):
+       with self.assertRaises(ValueError):
+           area(4, -5)
+   def test_perimeter_2(self):
+       with self.assertRaises(ValueError):
+           perimeter(4, 5, -6)
+
+   def test_area_3(self):
+       res = area(4, 0)
+       self.assertAlmostEqual(res, 0, delta=0.1)
+   def test_perimeter_3(self):
+       res = perimeter(4, 5, 0)
+       self.assertAlmostEqual(res, 9, delta=0.1)
+
+   def test_area_4(self):
+       res = area(4, 5.5)
+       self.assertAlmostEqual(res, 11, delta=0.1)
+   def test_perimeter_4(self):
+       res = perimeter(4, 5.5, 6)
+       self.assertAlmostEqual(res, 15.5, delta=0.1)
+
+
+
+

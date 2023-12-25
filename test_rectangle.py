21245169c8131f65670f9975a8fc1c3d9594c785
Mogohars <Qweqweqwe1232005@gmail.com>
2023-12-25 11:07:00 +0300

    rectangle test fix 2.0

--- test_rectangle.py
+++ test_rectangle.py
@@ -6,7 +6,7 @@ from rectangle import perimeter
 
 class RectangleTest(unittest.TestCase):
     def test_rectangle_area_1(self):
-        res = perimeter(0, 0)
+        res = perimeter(0, 5)
         self.assertEqual(res, 0)
 
     def test_rectangle_area_2(self):
@@ -23,7 +23,7 @@ class RectangleTest(unittest.TestCase):
             (area('5','5'))
 
     def test_rectangle_perimeter_1(self):
-        res = perimeter(5, 0)
+        res = perimeter(0, 0)
         self.assertEqual(res,0)
 
     def test_rectangle_perimeter_2(self):

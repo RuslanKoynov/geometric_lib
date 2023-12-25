57f1fa390927d3fbebe8873cabf5c5575ac7fe38
Rostyslav Suprunenko <142699779+Arxtarious@users.noreply.github.com>
2023-12-25 15:07:10 +0300

    Update Circle_unit_test.py

--- Circle_unit_test.py
+++ Circle_unit_test.py
@@ -15,7 +15,7 @@ class CircleTestCase(unittest.TestCase):
 
     def test_circle_positive_area(self):
         res = area(3)
-        self.assertAlmostEqual(res, 28.274)
+        self.assertAlmostEqual(res, 28.274, delta=0.001)
 
     def test_circle_other_data_type_area(self):
         self.assertRaises(TypeError, area, "123")
@@ -26,14 +26,14 @@ class CircleTestCase(unittest.TestCase):
 
     def test_circle_rational_perimeter(self):
         res = perimeter(3.5)
-        self.assertAlmostEqual(res, 21.991)
+        self.assertAlmostEqual(res, 21.991, delta=0.001)
 
     def test_circle_negative_perimeter(self):
         self.assertRaises(TypeError, perimeter, -3)
 
     def test_circle_positive_perimeter(self):
         res = perimeter(3)
-        self.assertAlmostEqual(res, 18.849)
+        self.assertAlmostEqual(res, 18.849,delta=0.001)
 
     def test_circle_other_data_type_perimeter(self):
         self.assertRaises(TypeError, perimeter, "123")

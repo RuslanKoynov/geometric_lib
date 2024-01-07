0e8830fef3655f47e88119a5205dd2d98ba07bf6
Руслан Самигуллин <144239367+rssolgaleo@users.noreply.github.com>
2024-01-07 05:15:07 +0500

    tests: nothing.

--- circle.py
+++ circle.py
@@ -1,15 +1,23 @@
 import unittest
 import math
 
+'''Принимает число r и возвращает площадь круга'''
+
 def area(r):
-    if r < 0:
-        raise ValueError("Radius cannot be negative")
     return math.pi * r * r
 
+'''Пример вызова:
+    result = area(5)
+    print(result) # Выведет 78.5'''
+
+'''Приннимает число r и возвращает периметр круга'''
+
 def perimeter(r):
-    if r < 0:
-        raise ValueError("Radius cannot be negative")
     return 2 * math.pi * r
+
+'''Пример вызова:
+    result = perimeter(5)
+    print(result) # Выведет 31.4'''
 
 class CircleTestCase(unittest.TestCase):
     def test_area_1(self):
@@ -29,13 +37,9 @@ class CircleTestCase(unittest.TestCase):
         self.assertAlmostEqual(res, 31.4, delta=0.1)
 
     def test_area_negative(self):
-        try:
+        with self.assertRaises(ValueError):
             area(-5)
-        except ValueError:
-            self.fail("Unexpected ValueError")
 
     def test_perimeter_negative(self):
-        try:
+        with self.assertRaises(ValueError):
             perimeter(-5)
-        except ValueError:
-            self.fail("Unexpected ValueError")

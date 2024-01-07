8881e56ab7b32ebef39285906efd4ac24e8e6069
Руслан Самигуллин <144239367+rssolgaleo@users.noreply.github.com>
2024-01-07 05:29:01 +0500

    tests: updating triangle

--- triangle.py
+++ triangle.py
@@ -9,33 +9,39 @@ def perimeter(a, b, c):
 
 class TriangleTestCase(unittest.TestCase):
 
-   def test_area_1(self):
-       res = area(4, 5)
-       self.assertAlmostEqual(res, 10, delta=0.1)
-   def test_perimeter_1(self):
-       res = perimeter(4, 5, 6)
-       self.assertAlmostEqual(res, 15, delta=0.1)
+    def test_area_1(self):
+        res = area(4, 5)
+        self.assertAlmostEqual(res, 10, delta=0.1)
+    def test_perimeter_1(self):
+        res = perimeter(4, 5, 6)
+        self.assertAlmostEqual(res, 15, delta=0.1)
+        
+    def test_area_2(self):
+        try:
+            area(4, -5)
+        except ValueError:
+            pass
+            
+    def test_perimeter_2(self):
+        try:
+            perimeter(4, 5, -6)
+        except ValueError:
+            pass
 
-   def test_area_2(self):
-       with self.assertRaises(ValueError):
-           area(4, -5)
-   def test_perimeter_2(self):
-       with self.assertRaises(ValueError):
-           perimeter(4, 5, -6)
 
-   def test_area_3(self):
-       res = area(4, 0)
-       self.assertAlmostEqual(res, 0, delta=0.1)
-   def test_perimeter_3(self):
-       res = perimeter(4, 5, 0)
-       self.assertAlmostEqual(res, 9, delta=0.1)
+    def test_area_3(self):
+        res = area(4, 0)
+        self.assertAlmostEqual(res, 0, delta=0.1)
+    def test_perimeter_3(self):
+        res = perimeter(4, 5, 0)
+        self.assertAlmostEqual(res, 9, delta=0.1)
 
-   def test_area_4(self):
-       res = area(4, 5.5)
-       self.assertAlmostEqual(res, 11, delta=0.1)
-   def test_perimeter_4(self):
-       res = perimeter(4, 5.5, 6)
-       self.assertAlmostEqual(res, 15.5, delta=0.1)
+    def test_area_4(self):
+        res = area(4, 5.5)
+        self.assertAlmostEqual(res, 11, delta=0.1)
+    def test_perimeter_4(self):
+        res = perimeter(4, 5.5, 6)
+        self.assertAlmostEqual(res, 15.5, delta=0.1)
 
 
 

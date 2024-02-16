3f58ec0d9ab51e7abb1f962abf194a9378187056
EzhNEesh <oyaiskander@yandex.ru>
2024-02-16 11:45:40 +0300

    test: circle tests separation

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -4,50 +4,51 @@ from contextlib import nullcontext as does_not_raise
 
 
 class TestCircle(unittest.TestCase):
-    def test_area(self):
-        eps = 0.1 ** 10
-        params = [
-            (0.001, 0.0000031415927, does_not_raise(), ''),
-            (1234, 4783879.062809778, does_not_raise(), ''),
-            (0, None, self.assertRaises(ValueError), 'The radius(a = 0) of a circle must be greater than zero'),
-            (-1, None, self.assertRaises(ValueError), 'The radius(a = -1) of a circle must be greater than zero'),
-            ('ab', None, self.assertRaises(TypeError), 'The radius of a circle must be int or float')
-        ]
-        for r, excepted, exc, exc_message in params:
-            with self.subTest():
-                with exc as context:
-                    res = circle.area(r)
-                if context is not None:
-                    self.assertEqual(
-                        exc_message,
-                        str(context.exception)
-                    )
-                else:
-                    self.assertTrue(
-                        excepted - eps <= res <= excepted + eps,
-                        f'{res}(result) != {excepted}(excepted result)'
-                    )
+    def test_area_integer(self):
+        res = circle.area(1234)
+        excepted = 4783879.062809778
 
-    def test_perimeter(self):
+        self.assertAlmostEqual(excepted, res, 6)
+
+    def test_area_float(self):
         eps = 0.1 ** 10
-        params = [
-            (0.001, 0.0062831853072, does_not_raise(), ''),
-            (1234, 7753.4506690596, does_not_raise(), ''),
-            (0, None, self.assertRaises(ValueError), 'The radius(a = 0) of a circle must be greater than zero'),
-            (-1, None, self.assertRaises(ValueError), 'The radius(a = -1) of a circle must be greater than zero'),
-            ('ab', None, self.assertRaises(TypeError), 'The radius of a circle must be int or float')
-        ]
-        for r, excepted, exc, exc_message in params:
-            with self.subTest():
-                with exc as context:
-                    res = circle.perimeter(r)
-                if context is not None:
-                    self.assertEqual(
-                        exc_message,
-                        str(context.exception)
-                    )
-                else:
-                    self.assertTrue(
-                        excepted - eps <= res <= excepted + eps,
-                        f'{res}(result) != {excepted}(excepted result)'
-                    )
+        res = circle.area(0.001)
+        excepted = 0.0000031415927
+
+        self.assertAlmostEqual(excepted, res, 6)
+
+    def test_area_zero(self):
+        with self.assertRaises(ValueError) as context:
+            circle.area(0)
+
+    def test_area_negative(self):
+        with self.assertRaises(ValueError) as context:
+            circle.area(-1)
+
+    def test_area_string(self):
+        with self.assertRaises(TypeError) as context:
+            circle.area('ab')
+
+    def test_perimeter_integer(self):
+        res = circle.perimeter(1234)
+        excepted = 7753.4506690596
+
+        self.assertAlmostEqual(excepted, res, 6)
+
+    def test_perimeter_float(self):
+        res = circle.perimeter(0.001)
+        excepted = 0.0062831853072
+
+        self.assertAlmostEqual(excepted, res, 6)
+
+    def test_perimeter_zero(self):
+        with self.assertRaises(ValueError) as context:
+            circle.perimeter(0)
+
+    def test_perimeter_negative(self):
+        with self.assertRaises(ValueError) as context:
+            circle.perimeter(-1)
+
+    def test_perimeter_string(self):
+        with self.assertRaises(TypeError) as context:
+            circle.perimeter('ab')

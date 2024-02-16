83c1f287a0c470766cf8b2c5549ab78c097cb497
EzhNEesh <oyaiskander@yandex.ru>
2024-02-16 17:31:49 +0300

    test: square tests separation

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,53 +1,50 @@
 import unittest
 import square
-from contextlib import nullcontext as does_not_raise
 
 
 class TestSquare(unittest.TestCase):
-    def test_area(self):
-        eps = 0.1 ** 10
-        params = [
-            (0.001, 0.000001, does_not_raise(), ''),
-            (1234, 1522756, does_not_raise(), ''),
-            (0, None, self.assertRaises(ValueError), 'The side(a = 0) of a square must be greater than zero'),
-            (-1, None, self.assertRaises(ValueError), 'The side(a = -1) of a square must be greater than zero'),
-            ('ab', None, self.assertRaises(TypeError), 'The side of a square must be int or float')
-        ]
-        for a, excepted, exc, exc_message in params:
-            with self.subTest():
-                with exc as context:
-                    res = square.area(a)
-                if context is not None:
-                    self.assertEqual(
-                        exc_message,
-                        str(context.exception)
-                    )
-                else:
-                    self.assertTrue(
-                        excepted - eps <= res <= excepted + eps,
-                        f'{res}(result) != {excepted}(excepted result)'
-                    )
+    def test_area_integer(self):
+        res = square.area(5)
+        excepted = 25
 
-    def test_perimeter(self):
-        eps = 0.1 ** 10
-        params = [
-            (0.001, 0.004, does_not_raise(), ''),
-            (1234, 4936, does_not_raise(), ''),
-            (0, None, self.assertRaises(ValueError), 'The side(a = 0) of a square must be greater than zero'),
-            (-1, None, self.assertRaises(ValueError), 'The side(a = -1) of a square must be greater than zero'),
-            ('ab', None, self.assertRaises(TypeError), 'The side of a square must be int or float')
-        ]
-        for a, excepted, exc, exc_message in params:
-            with self.subTest():
-                with exc as context:
-                    res = square.perimeter(a)
-                if context is not None:
-                    self.assertEqual(
-                        exc_message,
-                        str(context.exception)
-                    )
-                else:
-                    self.assertTrue(
-                        excepted - eps <= res <= excepted + eps,
-                        f'{res}(result) != {excepted}(excepted result)'
-                    )
+        self.assertEqual(excepted, res)
+
+    def test_area_float(self):
+        excepted = 0.0025
+
+        self.assertAlmostEqual(excepted, square.area(0.05), 6)
+
+    def test_area_zero_side(self):
+        with self.assertRaises(ValueError) as context:
+            square.area(0)
+
+    def test_area_negative_side(self):
+        with self.assertRaises(ValueError) as context:
+            square.area(-1)
+
+    def test_area_string_side(self):
+        with self.assertRaises(TypeError) as context:
+            square.area('ab')
+
+    def test_perimeter_integer(self):
+        res = square.perimeter(5)
+        excepted = 20
+
+        self.assertEqual(excepted, res)
+
+    def test_perimeter_float(self):
+        excepted = 0.2
+
+        self.assertAlmostEqual(excepted, square.perimeter(0.05), 6)
+
+    def test_perimeter_zero_side(self):
+        with self.assertRaises(ValueError) as context:
+            square.perimeter(0)
+
+    def test_perimeter_negative_side(self):
+        with self.assertRaises(ValueError) as context:
+            square.perimeter(-1)
+
+    def test_perimeter_string_side(self):
+        with self.assertRaises(TypeError) as context:
+            square.perimeter('ab')
